6314530de3f08d4dc3cb297c51c79509d68bf070
Hollin Wilkins <hrw7@cornell.edu>
2011-06-28 10:56:32 -0700

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Torri::Application.config.secret_token = '3917c4a9a8b46923a77838a23c47d593b48433d7f930021ff92df13941c7eee592960070472492477e22ff756efd848768edaeefc2fe201321aacfd96e15a071'
